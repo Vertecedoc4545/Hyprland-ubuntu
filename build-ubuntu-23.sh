ce9a9972f66bbb3bad20060fda97a271f1f6bd50
Yfgjøræn <58826034+Vertecedoc4545@users.noreply.github.com>
2023-04-30 21:30:47 -0600

    Release 1 of build script

--- build-ubuntu-23.sh
+++ build-ubuntu-23.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+build-from-nothing () {
+
 sudo apt-get install -y nala
-sudo nala install -y \ 
-meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr
+sudo nala install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr
 
 mkdir HyprSource
 cd HyprSource
@@ -16,13 +18,15 @@ wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.31/do
 tar -xvJf wayland-protocols-1.31.tar.xz
 
 wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.22.0/downloads/wayland-1.22.0.tar.xz
-tar -xzvJf wayland-1.22.0.tar.xz
+tar -xvJf wayland-1.22.0.tar.xz
 
 wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/downloads/libdisplay-info-0.1.1.tar.xz
 tar -xvJf libdisplay-info-0.1.1.tar.xz
 
 wget https://gitlab.freedesktop.org/emersion/libliftoff/-/releases/v0.4.1/downloads/libliftoff-0.4.1.tar.gz
 tar -xvf libliftoff-0.4.1.tar.gz
+
+## Building Wayland
 
 cd wayland-1.22.0
 mkdir build &&
@@ -37,6 +41,8 @@ sudo ninja install
 
 cd ../..
 
+## Building wayland-protocols
+
 cd wayland-protocols-1.31
 
 mkdir build &&
@@ -48,6 +54,8 @@ ninja
 sudo ninja install
 
 cd ../..
+
+## Building libdisplay-info
 
 cd libdisplay-info-0.1.1/
 
@@ -61,6 +69,8 @@ sudo ninja install
 
 cd ../..
 
+## Building libliftoff
+
 cd libliftoff-0.4.1/
 
 mkdir build &&
@@ -72,10 +82,33 @@ ninja
 sudo ninja install
 
 cd ../..
+}
+
+## Building Hyprland
+build-hyprland () {
+
+chmod a+rw hyprland-source
 
 cd hyprland-source/
 
 sed -i 's/\/usr\/local/\/usr/g' config.mk
 
+### Installing
 
 sudo make install
+
+#deletiing Everything
+
+cd ../..
+
+echo -e "\e[30m NOW YOU HAVE HYPRLAND INSTALLED!!! \e[0m"
+}
+
+if [ -d "HyprSource" ];
+then
+  cd HyprSource
+  build-hyprland
+else 
+  build-from-nothing
+  build-hyprland
+fi
